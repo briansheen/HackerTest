4a91fe1b6e5bc916ee8f41da3ecea700330bcbd7
Brian Sheen <bsheen@gmail.com>
2017-08-11 15:39:17 -0500

    solved palindrome

--- src/com/example/Main.java
+++ src/com/example/Main.java
@@ -7,24 +7,41 @@ public class Main {
 
 
     public static void main(String[] args) {
-        String inputString = "Mr John Smith    ";
-        int inputInt = 13;
-        String output = replaceString(inputString, inputInt);
-        System.out.println(output);
+        String inputString = "amen Icy CINEMA";
+        if (isPalindrome(inputString)) {
+            System.out.println("True");
+        } else {
+            System.out.println("False");
+        }
     }
 
-    public static String replaceString(String s, int n){
-        StringBuilder output = new StringBuilder();
-        for(int i = 0; i < n; ++i){
-            if(s.charAt(i)==' '){
-                output.append("%20");
-            } else {
-                output.append(s.charAt(i));
+    public static boolean isPalindrome(String s) {
+        StringBuilder reversed = new StringBuilder();
+        StringBuilder noSpace = new StringBuilder();
+        for (int i = 0; i < s.length(); ++i) {
+            if (s.charAt(i) != ' ') {
+                noSpace.append(s.charAt(i));
             }
         }
-        return output.toString();
+        String input = noSpace.toString();
+        input = input.toLowerCase();
+        System.out.println(input);
+        for (int i = s.length()-1; i >= 0; --i) {
+            if (s.charAt(i) != ' ') {
+                reversed.append(s.charAt(i));
+            }
+        }
+        String output = reversed.toString();
+        output = output.toLowerCase();
+        System.out.println(output);
+        if (input.equals(output)) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
+
 
 
 
