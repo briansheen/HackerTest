893a1689504558a4e482bcb563b51080f62607b1
Brian Sheen <bsheen@gmail.com>
2017-08-11 18:31:02 -0500

    solved StringCompression

--- src/com/example/Main.java
+++ src/com/example/Main.java
@@ -7,64 +7,34 @@ public class Main {
 
 
     public static void main(String[] args) {
-        String input = "pale";
-        String output = "bake";
-        if (isOneAway(input, output)) {
-            System.out.println("True");
+        String input = "abczzzzaaabbaaaccss";
+        System.out.println(stringCompress(input));
+        input = "abcc";
+        System.out.println(stringCompress(input));
+    }
+
+    public static String stringCompress(String s){
+        StringBuilder output = new StringBuilder();
+        int charCount = 1;
+        char c = s.charAt(0);
+        for(int i = 1; i < s.length(); ++i){
+            if(s.charAt(i)!=c) {
+                output.append(c);
+                output.append(charCount);
+                charCount=0;
+                c = s.charAt(i);
+            }
+            charCount++;
+            if(i==s.length()-1){
+                output.append(c);
+                output.append(charCount);
+            }
+        }
+        if(output.length()>=s.length()){
+            return s;
         } else {
-            System.out.println("False");
+            return output.toString();
         }
-    }
-
-    public static boolean isOneAway(String input, String output) {
-        if (input.compareTo(output) == 0) {
-            return true;
-        }
-        if((Math.abs(input.length()-output.length()))>=2){
-            return false;
-        }
-        if (input.length() == output.length()) {
-            return checkReplace(input, output);
-        }
-        if (input.length() > output.length()) {
-            return checkInsert(input, output);
-        }
-        if (output.length() > input.length()) {
-            return checkInsert(output, input);
-        }
-        return false;
-    }
-
-    public static boolean checkReplace(String input, String output) {
-        int replaceCount = 0;
-        for(int i = 0; i < input.length(); ++i){
-            if(input.charAt(i)!=output.charAt(i)){
-                replaceCount++;
-                if(replaceCount > 1){
-                    return false;
-                }
-            }
-        }
-        return true;
-    }
-
-    public static boolean checkInsert(String longer, String shorter){
-        int insertCount = 0;
-        int shorterIndex = 0;
-        for(int i = 0; i < longer.length(); ++i){
-            if(longer.length()-shorterIndex==1){
-                return true;
-            }
-            if(longer.charAt(i)!=shorter.charAt(shorterIndex)){
-                insertCount++;
-                i++;
-                if(insertCount > 1){
-                    return false;
-                }
-            }
-            shorterIndex++;
-        }
-        return true;
     }
 
 }
